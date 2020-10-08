784c358b405a68063685cd71554fb9eaccd831d2
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2020-10-08 19:02:45 -0300

    Add Home css

--- src/components/screens/Home/index.jsx
+++ src/components/screens/Home/index.jsx
@@ -5,7 +5,17 @@ class Home extends React.Component {
     render() {
         return(
             <div className="containerHome">
-                
+                <div className="card">
+                    <div className="student">
+                        <p>Alumno</p>
+                        <p>Fausto Azzaretti</p>
+                        <p>2020</p>
+                    </div>
+                    <div className="buttons">
+                        <button>Editar Informacion</button>
+                        <button>Contador</button>
+                    </div>
+                </div>
             </div>
         )
     }
