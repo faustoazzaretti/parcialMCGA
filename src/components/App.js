3af27bf9cf24d6bc641f7104d298ebed6fdcf32d
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2020-10-08 18:39:11 -0300

    Add Home component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './screens/Home/index';
+
+class App extends React.Component {
+  render() {
+    return(
+      <Home/>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/screens/Home/index.jsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div className="containerHome">
+                
+            </div>
+        )
+    }
+}
+
+export default Home;
