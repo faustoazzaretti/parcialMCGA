ca8dccc0ea38ff1e928b15546696ab6f81991d5a
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2020-10-14 14:47:20 -0300

    Updates

---
+++ precache-manifest.0ab1feed6c4e29454f17b99db3d3b717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8c7cb8435c52c950299dd5ba6779e5",
+    "url": "/parcialMCGA/index.html"
+  },
+  {
+    "revision": "0338112f852dce0b3224",
+    "url": "/parcialMCGA/static/css/main.4f259f75.chunk.css"
+  },
+  {
+    "revision": "428b7b1feffeed0b145c",
+    "url": "/parcialMCGA/static/js/2.26ca5088.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/parcialMCGA/static/js/2.26ca5088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0338112f852dce0b3224",
+    "url": "/parcialMCGA/static/js/main.63119cbe.chunk.js"
+  },
+  {
+    "revision": "dc6ac3c2b18b11e801d4",
+    "url": "/parcialMCGA/static/js/runtime-main.24699c62.js"
+  }
+]);
